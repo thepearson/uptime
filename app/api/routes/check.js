32c1c5c72a1db078020ee57f072b86d4f8bf7c58
thepearson <thepearson@gmail.com>
2017-02-08 16:42:50 +1300

    Tidied up the api files

--- app/api/routes/check.js
+++ app/api/routes/check.js
@@ -18,46 +18,70 @@ module.exports = function(app) {
     if (req.query.tag) {
       query.tags = req.query.tag;
     }
+    
     Check
-    .find(query)
-    .sort({ isUp: 1, lastChanged: -1 })
-    .exec(function(err, checks) {
-      if (err) return next(err);
-      res.json(checks);
-    });
+      .find(query)
+      .sort({ isUp: 1, lastChanged: -1 })
+      .exec(function(err, checks) {
+        if (err) {
+          return next(err);
+        }
+
+        res.json(checks);
+      });
   });
+
+
 
   app.get('/checks/needingPoll', function(req, res, next) {
     Check
-    .needingPoll()
-    .select({qos: 0})
-    .exec(function(err, checks) {
-      if (err) return next(err);
-      res.json(checks);
-    });
+      .needingPoll()
+      .select({qos: 0})
+      .exec(function(err, checks) {
+        if (err) {
+          return next(err);
+        }
+
+        res.json(checks);
+      });
   });
+
+
 
   // check route middleware
   var loadCheck = function(req, res, next) {
     Check
-    .find({ _id: req.params.id })
-    .select({qos: 0})
-    .findOne(function(err, check) {
-      if (err) return next(err);
-      if (!check) return res.json(404, { error: 'failed to load check ' + req.params.id });
-      req.check = check;
-      next();
-    });
+      .find({ _id: req.params.id })
+      .select({qos: 0})
+      .findOne(function(err, check) {
+        if (err) {
+          return next(err);
+        }
+
+        if (!check) {
+          return res.json(404, { error: 'failed to load check ' + req.params.id });
+        }
+
+        req.check = check;
+        next();
+      });
   };
+
+
 
   app.get('/checks/:id', loadCheck, function(req, res, next) {
     res.json(req.check);
   });
-  
+
+
+
   app.get('/checks/:id/pause', loadCheck, function(req, res, next) {
     req.check.togglePause();
     req.check.save(function(err) {
-      if (err) return next(new Error('failed to toggle pause on check' + req.params.id));
+      if (err) {
+        return next(new Error('failed to toggle pause on check' + req.params.id));
+      }
+
       new CheckEvent({
         timestamp: new Date(),
         check: req.check,
@@ -68,27 +92,45 @@ module.exports = function(app) {
     });
   });
 
+
+
   app.put('/check/:id/test', function (req, res, next) {
     Check.update({ _id: req.params.id }, { lastTested: new Date() }, function(err, numberAffected) {
-      if (err) return next(err);
+      if (err) {
+        return next(err);
+      }
+
       res.json({ numberAffected: numberAffected });
     });
   });
 
+
+
   app.get('/checks/:id/stat/:period/:timestamp', loadCheck, function(req, res, next) {
     req.check.getSingleStatForPeriod(req.params.period, new Date(parseInt(req.params.timestamp)), function(err, stat) {
-      if (err) return next(err);
+      if (err) {
+        return next(err);
+      }
+
       res.json(stat);
     });
   });
-  
+
+
+
+
   app.get('/checks/:id/stats/:type', loadCheck, function(req, res, next) {
     req.check.getStatsForPeriod(req.params.type, req.query.begin, req.query.end, function(err, stats) {
-      if(err) return next(err);
+      if (err) {
+        return next(err);
+      }
+
       res.json(stats);
     });
   });
-  
+
+
+
   app.get('/checks/:id/events', function(req, res, next) {
     var query = {
       check: req.params.id,
@@ -98,53 +140,79 @@ module.exports = function(app) {
       query.timestamp.$lte = req.query.end;
     }
     CheckEvent
-    .find(query)
-    .sort({ timestamp: -1 })
-    .select({tags: 0})
-    .limit(100)
-    .exec(function(err, events) {
-      if (err) return next(err);
-      CheckEvent.aggregateEventsByDay(events, function(err, aggregatedEvents) {
-        if(err) return next(err);
-        res.json(aggregatedEvents);
+      .find(query)
+      .sort({ timestamp: -1 })
+      .select({tags: 0})
+      .limit(100)
+      .exec(function(err, events) {
+        if (err) {
+          return next(err);
+        }
+
+        CheckEvent.aggregateEventsByDay(events, function(err, aggregatedEvents) {
+          if (err) {
+            return next(err);
+          }
+          res.json(aggregatedEvents);
+        });
       });
-    });
   });
+
+
 
  app.put('/checks', function(req, res, next) {
    var check = new Check();
    try {
      check.populateFromDirtyCheck(req.body, app.get('pollerCollection'));
      app.emit('populateFromDirtyCheck', check, req.body, check.type);
-   } catch (checkException) {
+   }
+   catch (checkException) {
      return next(checkException);
    }
+
    check.save(function(saveError) {
-     if(saveError) return next({status:500, error: saveError});
+     if (saveError) {
+       return next({status:500, error: saveError});
+     }
+
      res.json(check);
    });
  });
 
+
+
  app.delete('/checks/:id', loadCheck, function (req, res, next) {
   req.check.remove(function(err) {
-    if (err) return next(err);
+    if (err) {
+      return next(err);
+    }
     res.end();
   });
  });
 
+
+
  app.post('/checks/:id', function(req, res, next) {
    Check.findOne({ _id: req.params.id }, function(err, check) {
-     if (err) return next({status:500, error: err});
-     if (!check) return next({status:404, error: 'failed to load check ' + req.params.id})
+     if (err) {
+       return next({status:500, error: err});
+     }
+
+     if (!check) {
+       return next({status:404, error: 'failed to load check ' + req.params.id})
+     }
 
      try {
        check.populateFromDirtyCheck(req.body, app.get('pollerCollection'));
        app.emit('populateFromDirtyCheck', check, req.body, check.type);
-     } catch (checkException) {
+     }
+     catch (checkException) {
        return next(checkException);
      }
      check.save(function(saveError) {
-       if(saveError) return next({status:500, error: saveError});
+       if (saveError) {
+         return next({status:500, error: saveError});
+       }
        res.json(check);
      });
    });
